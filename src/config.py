f32bf925fa7a0c5c46d99ff7936521eab032c3d7
Yang Zhao-TU <101744236+Yang-Zhao-CIS-TU@users.noreply.github.com>
2024-04-09 15:37:36 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,14 @@
 config = {
+	"azure": {
 	"api_type": "azure",
 	"api_base": "YOUR_API_BASE_URL",
 	"api_version": "2023-07-01-preview",
 	"api_key": "YOUR_API_KEY"
+	},
+    "hugging_face": {
+	"api_type": "hugging_face",
+        "api_base": "https://api.huggingface.co",
+        "api_version": "v1",
+        "api_key": "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" 
+    }
 }
